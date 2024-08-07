8e0343b98ad342926ebeb55bd32be37993acadd0
Matthew Yu <matthewyu30121@gmail.com>
2024-08-06 20:20:57 -0400

    Made mobile experience a bit better.  Added Hack the 6ix project !

--- src/projects/projects.jsx
+++ src/projects/projects.jsx
@@ -7,6 +7,7 @@ import placeholderchecklist from './placeholderchecklist.png';
 import PTC from './PTC.png';
 import miliblock from './miliblock.png';
 import debg from './DEbackground.png';
+import workstation from './workstation.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import {faLinkedin, faGithub, faInstagram, faStrava } from '@fortawesome/free-brands-svg-icons';
@@ -61,13 +62,14 @@ return (
 
             </div> */}
             <div className = 'column'>
+            <Project img = {workstation} title = 'Focus Friend' description = 'At Hack the 6ix, we used React Native + Expo, arduinos, operational amplifiers, etc... to create an electroencephalogram ( EEG ) to determine if one is focused or not.' link = 'https://github.com/Yourself1011/focus-friend'/>
             <Project img  ={lemniscate} title = 'Lemniscate' description = 'A collaborative note-taking website utilizing famous, organized note-taking templates.' link ='https://github.com/CodingMayus/lemniscate'/>
             <Project img = {mentalhealth} title = 'Mental Health App' description = 'Accessing OpenAPI to create a friend you can talk to.' link='https://github.com/CodingMayus/Mental-Health-PSA'/>
             <Project img = {placeholderchecklist} title = 'Checklist Chrome Extension'description = 'Using Chrome.Storage API to create a personal TODO list.' link = 'https://github.com/CodingMayus/Check-List'/>
-
+         
             </div>
             <div className =  'column'>
-            <Project img = {debg} title = 'DE Background' description = 'In the SHAD program, we came up with a solution to a specific part of the Green Energy issue affecting Youth.' link = 'https://github.com/viridis-expo/viridis-expo.github.io'/>
+            <Project img = {debg} title = 'Design Project' description = 'In the SHAD program, we came up with a solution to a specific part of the Green Energy issue affecting Youth.' link = 'https://github.com/viridis-expo/viridis-expo.github.io'/>
             <Project img = {PTC} title = 'Sea Debris Ocean Promotion' description = ' Using Leaflet Js as a medium, predict where ocean trash will likely end up on a map.' link = 'https://github.com/CodingMayus/PTC'/>
             <Project img  ={miliblock} title = 'Miliblock' description  = 'Personalizable website blocker chrome extension that motivates you. ' link = 'https://github.com/CodingMayus/chrome-extension'/>
 
